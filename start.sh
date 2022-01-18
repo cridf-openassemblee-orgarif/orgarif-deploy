17f0922a447371844ba99fd14fa8d4f1888a896c
Mathieu Lorber <mathieu.lorber@gmail.com>
2022-01-18 16:53:28 +0100

    SAVEPOINT

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 source env.sh
-git reset --hard origin/$ENV
+source secrets.sh
+#git reset --hard origin/$ENV
 # kill previous one ?
-java -Dspring.profiles.active=dev,local-mlo -jar orgarif-server.jar
+#java -jar orgarif-server.jar
+java -Dspring.profiles.active=prod -jar orgarif-server.jar
