d962d983ff44a09aa94faef4744d4b577f80435b
Mathieu Lorber <mathieu.lorber@gmail.com>
2024-04-30 15:52:04 +0200

    SAVEPOINT

--- env.sh
+++ env.sh
@@ -1,5 +1,2 @@
 export ENV=prod
-export ORGARIF_POSTGRESQL_HOST=mut-p-pgr1.iledefrance.fr
-export ORGARIF_POSTGRESQL_PORT=5432
-export ORGARIF_POSTGRESQL_DB=orgarif
-export ORGARIF_POSTGRESQL_USER=orgarif
+
